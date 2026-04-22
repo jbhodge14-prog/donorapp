f37f61144517ff9a80ea2628d0a0c99a0e9690b1
Jesse Hodge <jbhodge14@gmail.com>
2026-04-22 09:12:36 -0500

    Fix Stripe API version

--- lib/integrations.ts
+++ lib/integrations.ts
@@ -8,7 +8,7 @@ export interface SyncResult {
 }
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-03-31.basil",
+  apiVersion: "2025-04-30.basil",
 });
 
 export async function createStripeCheckoutDonation(input: {
